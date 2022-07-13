7442ca2ee5969891bcf4dd969fe74385bba454d8
dannnnny12 <behappydanny1@gmail.com>
2022-07-13 23:07:35 +0800

    using dataframe to analysis

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from nltk.corpus import stopwords
 nltk.download('stopwords')
 nltk_stopwords = nltk.corpus.stopwords.words('english')
-newStpWord = ['game','Game']
+newStpWord = ['game','Game', ',','–','!','"','#','$','%','&',')','*','+','&',"i'm","I'm","I've","i've"]
 nltk_stopwords.extend(newStpWord)
 from nltk.tokenize import word_tokenize
 from textblob import TextBlob
@@ -17,6 +17,7 @@ from gensim.corpora.dictionary import Dictionary
 from gensim.models import LdaModel
 from gensim import models
 from pprint import pprint
+import pandas as pd
 result_list = []
 for page in range(1,4): #hetch page1 to 3
     response = requests.get("https://www.coursera.org/learn/java-programming/reviews?page="+str(page))
@@ -33,50 +34,38 @@ for page in range(1,4): #hetch page1 to 3
         #result = str(result)
         #word_tokenize(result)
         result_list.append(result.getText())
-    #print(result_list)
+#print(result_list)
+    
 arr = []
 pos = []
 neg = []
 for ite in range(len(result_list)): #tokenization
-    arr.append(word_tokenize(result_list[ite]))
+    arr.append(result_list[ite])
+#print(arr)
 #print(arr)
 
-for n in range(len(arr)): #stop words removal
-    for element in arr[n]:
-        if element in nltk_stopwords:
-            pass
-        else:
-            if TextBlob(element).sentiment.polarity>0.15:
-                pos.append(element)
-            elif TextBlob(element).sentiment.polarity<-0.15:
-                neg.append(element)
-
-            #stpwd_arr.append(element)
-#print(stpwd_arr)
-#print("positive words:"+str(pos))
-#print("negative words:"+str(neg))
-
-#Positive Reviews LDA Analysis
-id2word_pos = corpora.Dictionary([pos])
-#print(id2word)
-texts = pos
-corpus_pos = [id2word_pos.doc2bow(text.split()) for text in pos]
-#print(corpus)
-#print(corpus[:1][0][:30])
-num_topics = 10
-lda_model_pos = models.LdaModel(corpus=corpus_pos,id2word=id2word_pos,num_topics=num_topics)
-topic_list_pos = lda_model_pos.print_topics(10)
-print("10 positive topic distribution：\n")
-for topic in topic_list_pos:
-    print(topic)
-
-
-#Negative Reviews LDA Analysis
-id2word_neg = corpora.Dictionary([neg])
-#print(id2word)
-corpus_neg = [id2word_neg.doc2bow(text.split()) for text in neg]
-lda_model_neg = models.LdaModel(corpus=corpus_neg,id2word=id2word_neg,num_topics=num_topics)
-topic_list_neg = lda_model_neg.print_topics(10)
-print("10 negative topic distribution：\n")
-for topic in topic_list_neg:
-    print(topic)
+pos_tweets = [('I love this car'),
+    ('This view is amazing'),
+    ('I feel great this morning'),
+    ('I am so excited about the concert'),
+    ('He is my best friend')]
+def txt_sentiment(txt): 
+    '''Utility function to classify sentiment of passed tweet using textblob's sentiment method'''
+    # create TextBlob object of passed tweet text 
+    analysis = TextBlob(txt) 
+    return analysis.sentiment
+    # set sentiment 
+    if analysis.sentiment.polarity > 0.15:
+        return 'positive'
+    elif analysis.sentiment.polarity <-0.15: 
+        return 'negative'
+    else: 
+        return 'neutral'
+test = pd.DataFrame(result_list)
+test.columns = ["reviews"]
+test["reviews"] = test["reviews"].str.lower()
+test['review_withoutSTPWD'] = test['reviews'].apply(lambda x: ' '.join([word for word in x.split() if word not in (nltk_stopwords)]))
+test['sentiment'] = test['review_withoutSTPWD'].apply(lambda x: txt_sentiment(' '.join(x)))
+#pprint(test)
+#with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+print(test)
